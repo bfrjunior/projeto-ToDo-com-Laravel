0298bcc168a3a2bda14df52cc3880d600f865314
Fernando Rocha <beneditofernandor@gmail.com>
2023-08-17 12:57:44 -0300

    criando.tela.inclusão.de.tasks

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,5 +6,28 @@
 
     </x-slot:btn>
 
-    <section id=""> </section>
+    <section id="create_task_section">
+        <h1>Criar Tarefa</h1>
+        <form>
+            <div class="inputArea">
+                <label for="title">Titulo da Task</label>
+                <input id="title" name="title" placeholder="Digite o título da tarefa" required>
+            </div>
+            <div class="inputArea">
+                <label for="due_date">Data de Realização</label>
+                <input name="due_date" id="due_date" type="date" placeholder="" required>
+            </div>
+            <div class="inputArea">
+                <label for="category">Categoria</label>
+                <select id="category" name="category" required>
+                    <option selected disable value="">Selecione a categoria</option>
+                    <option>Valor qualquer</option>
+                </select>
+            </div>
+            <div class="inputArea">
+                <label for="title">Descrição da tarefa</label>
+                <textarea name="description" placeholder="Digite uma descrição para sua tarefa"></textarea>
+            </div>
+        </form>
+    </section>
 </x-layout>
